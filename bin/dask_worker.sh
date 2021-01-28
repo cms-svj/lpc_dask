0e056c398c1a09b70b4d5a6083b05bc19976bf7b
Kevin Pedro <kpedro88@gmail.com>
2021-01-28 15:32:00 -0600

    pin versions, add hook for patching venv

--- bin/dask_worker.sh
+++ bin/dask_worker.sh
@@ -2,23 +2,28 @@
 
 LCG=/cvmfs/sft.cern.ch/lcg/views/LCG_96python3/x86_64-centos7-gcc8-opt
 source ${LCG}/setup.sh
-python -m venv dask_venv
-source dask_venv/bin/activate
+VENV=dask_venv
+python -m venv ${VENV}
+source ${VENV}/bin/activate
 
 export PYTHONPATH=""
-export PATH=${PWD}/dask_venv/bin:${PATH}
+export PATH=${PWD}/${VENV}/bin:${PATH}
 
 pypackages=lib/python3.6/site-packages/
 lcgprefix=${LCG}/${pypackages}
 
-ln -sf ${lcgprefix}/pyxrootd dask_venv/${pypackages}/pyxrootd
-ln -sf ${lcgprefix}/XRootD dask_venv/${pypackages}/XRootD
+ln -sf ${lcgprefix}/pyxrootd ${VENV}/${pypackages}/pyxrootd
+ln -sf ${lcgprefix}/XRootD ${VENV}/${pypackages}/XRootD
 
 pip install --upgrade pip wheel --no-cache-dir
-pip install dask[dataframe] distributed coffea[dask]==0.6.47 --no-cache-dir
+pip install dask[dataframe]==2020.12.0 distributed==2020.12.0 coffea[dask]==0.6.47 --no-cache-dir
 pip list
 
-echo "Dask worker environment created at ${PWD}/dask_venv"
+echo "Dask worker environment created at ${PWD}/${VENV}"
+
+if [ -f patch.sh ]; then
+	./patch.sh ${VENV}
+fi
 
 /bin/pwd
 /bin/uname -a
